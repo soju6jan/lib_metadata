c31d9a8474c591c55e8e1bd1f940ab399aaa3c12
soju6jan <cybersol@naver.com>
2021-03-21 21:12:58 +0900

    1

--- server_util.py
+++ server_util.py
@@ -31,11 +31,15 @@ class MetadataServerUtil(object):
         try:
             from framework import py_urllib
             #url = '{server_plugin_ddns}/server/normal/metadata/get?code={code}'.format(server_plugin_ddns=server_plugin_ddns, code=code)
-            url = '{server_web}/meta/get_meta.php?type=meta&code={code}'.format(server_web=server_web, code=code)
+            #url = '{server_web}/meta/get_meta.php?type=meta&code={code}'.format(server_web=server_web, code=code)
+            url = '{server_web}/meta/get_meta.php?'.format(server_web=server_web)
+            url += py_urllib.urlencode({'type':'meta', 'code':code})
             data = requests.get(url).json()
             if data['ret'] == 'success':
                 return data['data']
-        except Exception as exception: 
+        except Exception as exception:
+            logger.debug('Exception:%s', exception)
+            logger.debug(traceback.format_exc())
             logger.error('metaserver connection fail.. get_metadata')
     
     """
@@ -116,7 +120,9 @@ class MetadataServerUtil(object):
         try:
             from framework import py_urllib
             #url = '{server_plugin_ddns}/server/normal/meta_extra/get?code={code}'.format(server_plugin_ddns=server_plugin_ddns, code=code)
-            url = '{server_web}/meta/get_meta.php?type=extra&code={code}'.format(server_web=server_web, code=code)
+            #url = '{server_web}/meta/get_meta.php?type=extra&code={code}'.format(server_web=server_web, code=py_urllib.quote(code))
+            url = '{server_web}/meta/get_meta.php?'.format(server_web=server_web)
+            url += py_urllib.urlencode({'type':'extra', 'code':code})
             data = requests.get(url).json()
             if data['ret'] == 'success':
                 return data['data']
