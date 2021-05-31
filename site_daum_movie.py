8d7e1864511aa7077db0d40517f7e33328469efa
soju6jan <cybersol@naver.com>
2021-05-31 11:49:58 +0900

    다음 영화 검색 수 15개로 변경. 와일드 (2014)

--- site_daum_movie.py
+++ site_daum_movie.py
@@ -57,7 +57,7 @@ class SiteDaumMovie(SiteDaum):
             for idx, item in enumerate(data['result']['search_result']['documents']):
                 item = item['document']
                 logger.debug(item)
-                if idx > 5:
+                if idx > 15:
                     break
                 entity = EntitySearchItemMovie(cls.site_name)
                 entity.title = item['titleKoreanHanl']
