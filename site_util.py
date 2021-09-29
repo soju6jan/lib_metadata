2cbfaf98241614f1e936bff571f3e3ae04059f65
105PM <105pm.dev@gmail.com>
2021-09-29 23:26:38 +0900

    header 변경, avdbs seq 추가

--- site_util.py
+++ site_util.py
@@ -18,10 +18,10 @@ class SiteUtil(object):
     session = requests.Session()
 
     default_headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
         'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         'Accept-Language' : 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Cookie' : 'over18=1;age_check_done=1;',
+        # 'Cookie' : 'over18=1;age_check_done=1;',
     } 
 
     @classmethod 
