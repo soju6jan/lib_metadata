8389a5a79df9fe655fe3b20fc12f95e019745f80
105PM <105pm.dev@gmail.com>
2021-12-22 15:49:52 +0900

    add 10musume

--- __init__.py
+++ __init__.py
@@ -36,6 +36,7 @@ from .site_mgstage import SiteMgstageDvd, SiteMgstageAma
 from .site_vibe import SiteVibe
 
 from .site_uncensored.site_1pondotv import Site1PondoTv
+from .site_uncensored.site_10musume import Site10Musume
 
 from .site_fc2.site_7mmtv import Site7mmTv
 from .site_fc2.site_bp4x import SiteBp4x
