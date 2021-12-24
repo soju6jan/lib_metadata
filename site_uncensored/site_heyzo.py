6b89b8f188bdd0629be4420082582d6fea25f8a1
105PM <105pm.dev@gmail.com>
2021-12-25 04:27:45 +0900

    update heyzo

--- site_uncensored/site_heyzo.py
+++ site_uncensored/site_heyzo.py
@@ -2,6 +2,7 @@
 import requests, re, json
 import traceback
 from dateutil.parser import parse
+import unicodedata
 
 from lxml import html
 
@@ -21,7 +22,7 @@ ModelSetting = P.ModelSetting
 
 class SiteHeyzo(object):
     site_name = 'heyzo'
-    site_base_url = 'https://m.heyzo.com'
+    site_base_url = 'https://www.heyzo.com'
     module_char = 'E'
     site_char = 'H'
 
@@ -32,6 +33,7 @@ class SiteHeyzo(object):
             if re.search('(\\d{4})', keyword, re.I) is not None and 'heyzo' in keyword.lower():
                 code = re.search('(\\d{4})', keyword, re.I).group()
             else:
+                # logger.debug(f'invalid keyword: {keyword}')
                 ret['ret'] = 'failed'
                 ret['data'] = 'invalid keyword'
                 return ret
@@ -39,7 +41,7 @@ class SiteHeyzo(object):
             url = f'{cls.site_base_url}/moviepages/{code}/index.html'
 
             if SiteUtil.get_response(url, proxy_url=proxy_url).status_code == 404:
-                logger.debug(f'not found: {keyword}')
+                # logger.debug(f'not found: {keyword}')
                 ret['ret'] = 'failed'
                 ret['data'] = 'not found'
                 return ret
@@ -51,10 +53,24 @@ class SiteHeyzo(object):
             item = EntityAVSearch(cls.site_name)
             item.code = cls.module_char + cls.site_char + code
 
-            item.title = item.title_ko = tree.xpath('//div[@id="container"]/h1/text()')[0].strip()
-            item.year = parse(tree.xpath('//*[@id="moviedetail"]/div[2]/span/text()')[1].strip()).date().year
+            # json이 있는 경우, 없는 경우
+            tmp = {}
+            try:
+                json_data = json.loads(re.sub('(\"\")\w.', '\"', tree.xpath('//*[@id="movie"]/script[@type="application/ld+json"]/text()')[0]), strict=False)
+                tmp['title'] = unicodedata.normalize('NFKC', json_data['name'])
+                tmp['year'] = parse(json_data['dateCreated']).date().year
+                tmp['image_url'] = f'https:{json_data["image"]}'
+            except:
+                m_tree = SiteUtil.get_tree(url.replace('www.', 'm.'), proxy_url=proxy_url)
+                tmp['title'] = m_tree.xpath('//div[@id="container"]/h1/text()')[0].strip()
+                tmp['year'] = parse(m_tree.xpath('//*[@id="moviedetail"]/div[2]/span/text()')[1].strip()).date().year
+                tmp['image_url'] = f'https://m.heyzo.com/contents/3000/{code}/images/player_thumbnail.jpg'
 
-            item.image_url = f'https://m.heyzo.com/contents/3000/{code}/images/player_thumbnail.jpg'
+
+            item.title = item.title_ko = tmp['title']
+            item.year = tmp['year']
+
+            item.image_url = tmp['image_url']
             if manual == True:
                 if image_mode == '3':
                     image_mode = '0'
@@ -91,27 +107,59 @@ class SiteHeyzo(object):
             ret = {}
             url = f'{cls.site_base_url}/moviepages/{code[2:]}/index.html'
             tree = SiteUtil.get_tree(url, proxy_url=proxy_url)
+
+            # json이 있는 경우, 없는 경우
+            tmp = {}
+            try:
+                json_data = json.loads(re.sub('(\"\")\w.', '\"', tree.xpath('//*[@id="movie"]/script[@type="application/ld+json"]/text()')[0]), strict=False)
+                tmp['data_poster'] = f'https:{json_data["actor"]["image"]}'
+                tmp['data_landscape'] = f'https:{json_data["image"]}'
+                tmp['tagline'] = unicodedata.normalize('NFKC', json_data['name'])
+                tmp['premiered'] = str(parse(json_data['dateCreated']).date())
+                tmp['year'] = parse(json_data['dateCreated']).date().year
+                tmp['actorlist'] = tree.xpath('//div[@id="movie"]//table[@class="movieInfo"]//tr[@class="table-actor"]//span/text()')
+                tmp['genrelist'] = tree.xpath('//tr[@class="table-tag-keyword-small"]//ul[@class="tag-keyword-list"]//li/a/text()')
+                if json_data['description'] != '':
+                    tmp['plot'] = unicodedata.normalize('NFKC', json_data['description']).strip()
+                else:
+                    tmp['plot'] = tmp['tagline']
+
+            except:
+                m_tree = SiteUtil.get_tree(url.replace('www.', 'm.'), proxy_url=proxy_url)
+                tmp['data_poster'] = f'https://m.heyzo.com/contents/3000/{code[2:]}/images/thumbnail.jpg'
+                tmp['data_landscape'] = f'https://m.heyzo.com/contents/3000/{code[2:]}/images/player_thumbnail.jpg'
+                tmp['tagline'] = m_tree.xpath('//div[@id="container"]/h1/text()')[0].strip()
+                tmp['premiered'] = str(parse(m_tree.xpath('//*[@id="moviedetail"]/div[2]/span/text()')[1].strip()).date())
+                tmp['year'] = parse(m_tree.xpath('//*[@id="moviedetail"]/div[2]/span/text()')[1].strip()).date().year
+                tmp['actorlist'] = m_tree.xpath('//*[@id="moviedetail"]/div[1]/strong/text()')[0].strip().split()
+                tmp['genrelist'] = m_tree.xpath('//*[@id="keyword"]/ul//li/a/text()')
+                try:
+                    tmp['plot'] = m_tree.xpath('//*[@id="memo"]/text()')[0]
+                except:
+                    tmp['plot'] = tmp['tagline']
+            
+
             entity = EntityMovie(cls.site_name, code)
             entity.country = [u'일본']
             entity.mpaa = u'청소년 관람불가'
 
             # 썸네일
             entity.thumb = []
-            data_poster = SiteUtil.get_image_url(f'https://m.heyzo.com/contents/3000/{code[2:]}/images/thumbnail.jpg', image_mode, proxy_url=proxy_url)
+            data_poster = SiteUtil.get_image_url(tmp['data_poster'], image_mode, proxy_url=proxy_url)
             entity.thumb.append(EntityThumb(aspect='poster', value=data_poster['image_url']))
-            data_landscape = SiteUtil.get_image_url(f'https://m.heyzo.com/contents/3000/{code[2:]}/images/player_thumbnail.jpg', image_mode, proxy_url=proxy_url)
+            data_landscape = SiteUtil.get_image_url(tmp['data_landscape'], image_mode, proxy_url=proxy_url)
             entity.thumb.append(EntityThumb(aspect='landscape', value=data_landscape['image_url']))
 
             # tagline
-            entity.tagline = SiteUtil.trans(tree.xpath('//div[@id="container"]/h1/text()')[0].strip(), do_trans=do_trans)
+            entity.tagline = SiteUtil.trans(tmp['tagline'], do_trans=do_trans)
 
             # date, year
-            entity.premiered = str(parse(tree.xpath('//*[@id="moviedetail"]/div[2]/span/text()')[1].strip()).date())
-            entity.year = parse(tree.xpath('//*[@id="moviedetail"]/div[2]/span/text()')[1].strip()).date().year
+            entity.premiered = tmp['premiered']
+            entity.year = tmp['year']
 
             # actor
             entity.actor = []
-            for actor in tree.xpath('//*[@id="moviedetail"]/div[1]/strong/text()')[0].strip().split():
+            for actor in tmp['actorlist']:
                 entity.actor.append(EntityActor(actor))
 
 
@@ -125,7 +173,7 @@ class SiteHeyzo(object):
             # genre
             entity.genre = []
             genrelist = []
-            genrelist = tree.xpath('//*[@id="keyword"]/ul//li/a/text()')
+            genrelist = tmp['genrelist']
             if genrelist != []:
                 for item in genrelist:
                     entity.genre.append(SiteUtil.get_translated_tag('uncen_tags', item)) # 미리 번역된 태그를 포함
@@ -135,11 +183,16 @@ class SiteHeyzo(object):
             entity.title = entity.originaltitle = entity.sorttitle = f'HEYZO-{code[2:]}'
 
             # entity.ratings
-            try: entity.ratings.append(EntityRatings(float(tree.xpath('//*[@id="totalRate"]/script/text()')[0].split('\'')[1], max=5, name=cls.site_name, image_url='https://m.heyzo.com/images/star.png')))
-            except: pass
+            # try: 
+            #     entity.ratings.append(EntityRatings(float(tree.xpath('//*[@id="movie"]//span[@itemprop="ratingValue"]/text()')[0], max=5, name=cls.site_name)))
+            # except: pass
 
             # plot
-            entity.plot = SiteUtil.trans(tree.xpath('//*[@id="memo"]/text()')[0], do_trans=do_trans)
+            # 플롯 없는 경우도 있음
+            if tmp['plot'] != '':
+                entity.plot = SiteUtil.trans(tmp['plot'], do_trans=do_trans)
+            else:
+                entity.plot = ''
             
             # 팬아트
             # entity.fanart = []
@@ -149,7 +202,10 @@ class SiteHeyzo(object):
 
             # 부가영상 or 예고편
             entity.extras = []
-            entity.extras.append(EntityExtra('trailer', entity.title, 'mp4', 'https:'+re.search(r"\" src=\"(.*?)\"", tree.xpath('//*[@id="container"]/script[4]/text()')[0]).group(1), thumb=f'https://m.heyzo.com/contents/3000/{code[2:]}/images/player_thumbnail.jpg'))
+            try:
+                entity.extras.append(EntityExtra('trailer', entity.title, 'mp4', f'https://m.heyzo.com/contents/3000/{code[2:]}/sample.mp4', thumb=f'https://m.heyzo.com/contents/3000/{code[2:]}/images/player_thumbnail.jpg'))
+            except:
+                pass
 
             ret['ret'] = 'success'
             ret['data'] = entity.as_dict()
